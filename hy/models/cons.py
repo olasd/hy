da158f36311c58ee8f09ba2d82c8648606936f4a
Nicolas Dandrimont <nicolas.dandrimont@crans.org>
2014-01-05 20:40:02 +0100

    Don't collapse conses with unquotes

--- hy/models/cons.py
+++ hy/models/cons.py
@@ -23,6 +23,7 @@ import itertools
 from hy.macros import _wrap_value
 from hy.models import HyObject
 from hy.models.expression import HyExpression
+from hy.models.symbol import HySymbol
 
 
 class HyCons(HyObject):
@@ -36,6 +37,13 @@ class HyCons(HyObject):
 
     def __new__(cls, car, cdr):
         if isinstance(cdr, list):
+
+            # Keep unquotes in the cdr of conses
+            if type(cdr) == HyExpression:
+                if len(cdr) > 0 and type(cdr[0]) == HySymbol:
+                    if cdr[0] in ("unquote", "unquote_splice"):
+                        return super(HyCons, cls).__new__(cls)
+
             return cdr.__class__([_wrap_value(car)] + cdr)
 
         elif cdr is None:
